7b85293cf473137c9a43483f8cbea2cfc173d4c3
JZXHanta <havoyles@gmail.com>
2025-11-17 12:45:46 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from player import Player
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
